53e22251d798062923655335899a0df2bfaf36a8
Varadhan Venbakam Vengadam <vavenbak@microsoft.com>
2016-04-21 11:11:08 +0530

    first commit

---
+++ GitDeployTest/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GitDeployTest.Controllers
+{
+    public class HomeController : ApiController
+    {
+
+        [HttpGet]
+        public string Test()
+        {
+            return "Hello world";
+        }
+    }
+}
---
+++ GitDeployTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitDeployTest.Global" Language="C#" %>
